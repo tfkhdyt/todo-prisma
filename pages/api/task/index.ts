767c64025c89fd1303fb681d63c62f9af92c5ae1
tfkhdyt <tfkhdyt@pm.me>
2022-08-28 10:44:35 +0700

    fix(/api/task): fix post and get method

--- pages/api/task/index.ts
+++ pages/api/task/index.ts
@@ -1,15 +1,33 @@
 import { NextApiHandler } from 'next';
+import { getSession } from 'next-auth/react';
 
 import { prisma } from '../../../lib/prisma';
 
 const handler: NextApiHandler = async (req, res) => {
+  const session = await getSession({ req });
+
   if (req.method === 'GET') {
-    const tasks = await prisma.task.findMany();
+    const tasks = await prisma.task.findMany({
+      where: {
+        user: {
+          email: session?.user?.email as string,
+        },
+      },
+    });
     res.json(tasks);
     return;
   } else if (req.method === 'POST') {
+    const body = JSON.parse(req.body);
+
     const task = await prisma.task.create({
-      data: JSON.parse(req.body),
+      data: {
+        taskName: body.taskName,
+        user: {
+          connect: {
+            email: session?.user?.email as string,
+          },
+        },
+      },
     });
     res.json(task);
     return;

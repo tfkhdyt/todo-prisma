055815722e04d63555779ca57e65816dcfd06850
tfkhdyt <tfkhdyt@pm.me>
2022-08-28 09:50:04 +0700

    feat([...nextauth].ts): setup prisma adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import GoogleProviders from 'next-auth/providers/google';
 
+import { prisma } from '../../../lib/prisma';
+
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProviders({
       clientId: process.env.GOOGLE_ID as string,

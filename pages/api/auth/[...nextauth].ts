04e0d858ee1c4640422d26b0c4c2ad21ffa914f8
tfkhdyt <tfkhdyt@pm.me>
2022-08-28 10:43:50 +0700

    feat(nextauth): add github provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import GithubProviders from 'next-auth/providers/github';
 import GoogleProviders from 'next-auth/providers/google';
 
 import { prisma } from '../../../lib/prisma';
@@ -10,6 +11,10 @@ export default NextAuth({
     GoogleProviders({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    GithubProviders({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
 });

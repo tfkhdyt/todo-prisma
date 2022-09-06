996892ee2e446e59dfe18788211fda9149ed1bc4
tfkhdyt <tfkhdyt@pm.me>
2022-09-07 06:59:56 +0700

    feat(_app): add query client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,16 +6,19 @@ import {
 import { useHotkeys, useLocalStorage } from '@mantine/hooks';
 import { ModalsProvider } from '@mantine/modals';
 import { NotificationsProvider } from '@mantine/notifications';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
+import { FC } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
     key: 'mantine-color-scheme',
     defaultValue: 'light',
     getInitialValueInEffect: true,
   });
+  const queryClient = new QueryClient();
 
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
@@ -23,31 +26,32 @@ function MyApp({ Component, pageProps }: AppProps) {
   useHotkeys([['mod+J', () => toggleColorScheme()]]);
 
   return (
-    <SessionProvider session={pageProps.session}>
-      <ColorSchemeProvider
-        colorScheme={colorScheme}
-        toggleColorScheme={toggleColorScheme}
-      >
-        <MantineProvider
-          withGlobalStyles
-          withNormalizeCSS
-          theme={{
-            /** Put your mantine theme override here */
-            colorScheme,
-          }}
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={pageProps.session}>
+        <ColorSchemeProvider
+          colorScheme={colorScheme}
+          toggleColorScheme={toggleColorScheme}
         >
-          <NotificationsProvider>
-            <ModalsProvider>
-              <Head>
-                <title>To Do List</title>
-              </Head>
-              <Component {...pageProps} />
-            </ModalsProvider>
-          </NotificationsProvider>
-        </MantineProvider>
-      </ColorSchemeProvider>
-    </SessionProvider>
+          <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+              colorScheme,
+            }}
+          >
+            <NotificationsProvider>
+              <ModalsProvider>
+                <Head>
+                  <title>To Do List</title>
+                </Head>
+                <Component {...pageProps} />
+              </ModalsProvider>
+            </NotificationsProvider>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </SessionProvider>
+    </QueryClientProvider>
   );
-}
+};
 
 export default MyApp;

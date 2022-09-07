7bea3e9aec00a1bbec415f46e794968b726c73e8
tfkhdyt <tfkhdyt@pm.me>
2022-09-07 09:01:10 +0700

    fix(Loading): change to simple loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,10 @@
-import { LoadingOverlay } from '@mantine/core';
+import { Center, Loader } from '@mantine/core';
 
 function Loading() {
   return (
-    <div style={{ width: '100%', position: 'relative' }}>
-      <LoadingOverlay visible={true} overlayBlur={2} />
-    </div>
+    <Center>
+      <Loader />
+    </Center>
   );
 }
 
